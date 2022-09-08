e2e4863e09583451ffa42f6d1efd59c4ead818ee
Richard Mattinson <richard.m@ttinson.net>
2022-09-08 16:25:24 +0100

    token.js complete

--- src/token.js
+++ src/token.js
@@ -5,17 +5,22 @@ const jwt = require('jsonwebtoken');
  * 
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#jwtsignpayload-secretorprivatekey-options-callback
  */
-function createToken() {
-    
+function createToken(payload, secret) {
+    const newToken = jwt.sign(payload, secret)
+    return newToken
 }
+
+console.log(createToken());
+// node src/token.js
 
 /**
  * Implement this function to accept a payload, secret key and an expiry time, and return a JWT with an expiry
  * 
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#token-expiration-exp-claim
  */
-function createTokenWithExpiry() {
-    
+function createTokenWithExpiry(payload, secret, expiryTime) {
+    const newToken = jwt.sign(payload, secret, {expiresIn: expiryTime});
+    return newToken;
 }
 
 /**
@@ -23,9 +28,18 @@ function createTokenWithExpiry() {
  * 
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
  */
-function verifyToken() {
-
+function verifyToken(token, secret) {
+try {
+    const createdToken = jwt.verify(token, secret)
+    if(createdToken){ // if the token exists, the user must be authenticated
+        return createdToken
+    }
+} catch (err) {
+    return false
 }
+}
+
+console.log((verifyToken(createToken, secret)));
 
 module.exports = {
     createToken,
